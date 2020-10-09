60aec4913842bd1401eaf96f10c5bd1308c9c9e2
Doug Rosenberg <doug.rosenberg@gmail.com>
2020-10-08 21:50:40 -0500

    created movies.js

---
+++ models/movies.js
@@ -0,0 +1,9 @@
+// module.exports = function(sequelize, DataTypes) {
+//     var Movie = sequelize.define("Movie", {
+//         name: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         }
+//     })
+//     return Movie;
+// }
