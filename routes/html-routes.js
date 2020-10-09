38dba7756ee4531bf880884299cce1612f87ee5b
joolyen <julianivey@gmail.com>
2020-10-08 21:40:51 -0500

    implementing handlebars - now functioning

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,8 +10,8 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
+    res.render("index", { layout: "index" });
     res.sendFile(path.join(__dirname, "../public/signup.html"));
-    res.render('index', {layout : 'main'});
   });
 
   app.get("/login", (req, res) => {
