e9b165ad43f8b3786617708d1eeaf3c351b6cf28
Doug Rosenberg <doug.rosenberg@gmail.com>
2020-10-14 11:20:49 -0500

    refined code in login, api-routes, and front page all js

--- public/js/frontpage.js
+++ public/js/frontpage.js
@@ -8,18 +8,36 @@
 let searchButton = $("#search-btn")
 console.log("search-btn", searchButton);
 searchButton.on("click", function(event) {
-const searchValue = $("#search-bar").val().trim()
-console.log(searchValue)
-//get request with query string on end
-$.ajax({
-    url: `/api/moviesearch?value=${searchValue}`,
-    method: "GET",
-    }).then(function(res) {
-        //line 20 is the response i'm getting from my server
-//create cards for each one and append to the parent div
-//handle for misspelled errors
-        console.log(res);
+    const searchValue = $("#search-bar").val().trim()
+    // let signedIn = false
+    //get request with query string on end
+    // $.ajax({
+    //     url: `/api/user_data`,
+    //     method: "GET",
+    // }).then(res => {
+    //     if (!res){
+    //         console.log("no user logged in");
+    //         return;
+    //     }
+    //     signedIn = true
+    //     return;
+    // }).catch(err => console.log(err));
+    
+    // if (!signedIn){
+    //     console.log("request forbidden");
+    //     //TODO: redirect towards login page (window thingy)
+    // } else {
+        $.ajax({
+            url: `/api/moviesearch?value=${searchValue}`,
+            method: "GET",
+            }).then(function(res) {
+                //line 20 is the response i'm getting from my server
+        //create cards for each one and append to the parent div
+        //handle for misspelled errors
+                console.log(res);
+            });
+     //   } 
     });
-});
+ 
 
 
